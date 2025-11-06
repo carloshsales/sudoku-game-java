d412226ae5355901f365b4ec294e98df78641a62
Carlos Henrique <carlosh.sales@outlook.com>
2025-11-06 09:29:02 -0300

    feat: custom colors class

---
+++ src/com/sudoku/game/model/ui/SudokuColors.java
@@ -0,0 +1,15 @@
+package com.sudoku.game.model.ui;
+
+import java.awt.*;
+
+public class SudokuColors {
+    public static final Color BACKGROUND = Color.decode("#F0F4FA");
+    public static final Color GRID_LINE = Color.decode("#C8D3E1");
+    public static final Color BLOCK_BACKGROUND = Color.decode("#FFFFFF");
+    public static final Color TEXT = Color.decode("#1A2E47");
+    public static final Color GIVEN_NUMBER = Color.decode("#355C9A");
+    public static final Color PLAYER_NUMBER = Color.decode("#0D0D0D");
+    public static final Color SELECTED = Color.decode("#A8C5FF");
+    public static final Color ERROR = Color.decode("#F28B82");
+    public static final Color HOVER = Color.decode("#DDEAFE");
+}
